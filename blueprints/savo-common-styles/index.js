0c69d862671196c7c3bcb8e18ea1be05ec230689
Rob <robert.abby@savogroup.com>
2018-04-24 11:08:29 -0500

    changes

--- blueprints/savo-common-styles/index.js
+++ blueprints/savo-common-styles/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 /* eslint-env node */
 module.exports = {
   description: '',
@@ -13,21 +15,26 @@ module.exports = {
   },
 
   _insertImportIntoAppCss(options) {
-    let baseDir = options.dummy ? 'tests/dummy/app/styles' : 'app/styles';
+    // console.log('/options/', options);
+    console.log('/options.addon/', options.addon);
+    let baseDir = options.dummy ? 'tests/dummy/app/styles' : 'addon/styles';
+    console.log('/baseDir/', baseDir);
 
-    let appStylesFile = 'app.scss';
+    let appStylesFile = 'addon.scss';
     let text = "@import 'savo-common-styles/addon'";
 
     let appStylesPath = `${baseDir}/${appStylesFile}`;
+    console.log('/appStylesPath/', appStylesPath);
 
+    let contents = fs.readFileSync(appStylesPath, 'utf8');
+
+    if (!contents.match(text)) {
+      fs.writeFileSync(appStylesPath, `${text};\n\n${contents}`, 'utf8');
+    }
     // The import isn't necessary for addons, as tailwind will automatically
     // be concatenated into vendor.css
     if (fs.existsSync(appStylesPath)) {
-      let contents = fs.readFileSync(appStylesPath, 'utf8');
-
-      if (!contents.match(text)) {
-        fs.writeFileSync(appStylesPath, `${text};\n\n${contents}`, 'utf8');
-      }
+      console.log('/appStylesPath/ exists!');
     }
 
     // Couldn't get this to insert the text at the top of file
