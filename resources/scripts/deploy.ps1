40764e1c64e6fac37b84eeda3c217990ad89d5f1
Rob <robert.abby@savogroup.com>
2018-04-25 10:13:00 -0500

    New build script

--- resources/scripts/deploy.ps1
+++ resources/scripts/deploy.ps1
@@ -1,23 +1,7 @@
 [CmdletBinding()]
 param (
-  [Parameter(Mandatory = $True, Position = 1)]
-  [string]$buildLabel
+    [Parameter(Mandatory = $True, Position = 1)]
+    [string]$buildLabel
 )
 
-$published = Invoke-Expression "npm view . versions name --json" | ConvertFrom-Json
-$local = Invoke-Expression "npm version" | ConvertFrom-Json
-$name = $published.name
-
-$latest = $published.versions[-1]
-$current = $local.($published.name)
-
-Write-Host "package name      "$name
-Write-Host "latest version    "$latest
-Write-Host "current version   "$current
-
-if($latest -eq $current) {
-  Write-Host "CURRENT version matches LATEST version, NOT publishing new version"
-} else {
-  Write-Host "publishing new version" $current
-  . npm publish --tag="build-$buildLabel"
-}
+. npm publish --tag="build-$buildLabel"
